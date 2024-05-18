f2de54df933c6f9c4b1b7e74e28abd19aa840cd7
ahmed dacharoui <dachraouia193@gmail.com>
2024-05-18 19:29:18 +0100

    complete the client side

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,23 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ProduitRepository;
 
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index(Request $request): Response
+    public function index(Request $request,ProduitRepository $produitRepository): Response
     {
         $session = $request->getSession();
         $panier = $session->get('panier', []);
         $panierData = [];
+        $total = 0;
+
         foreach ($panier as $id => $quantity) {
-            $panierData[] = [
-                'id' => $id,
-                'quantity' => $quantity,
+            $product = $produitRepository->find($id);
+            $price = $product->getPrice();
+            $total += $price * $quantity;
+            if ($product) {
+                $panierData[] = [
+                    'id' => $product->getId(),
+                    'name' => $product->getName(),
+                    'price' => $price,
+                    'quantity' => $quantity,
+                   
+                ];
                 
-            ];
+            }
         }
-        return $this->render('cart/index.html.twig', ['items' => $panierData]);
+    
+        return $this->render('cart/index.html.twig', [
+            'items' => $panierData,
+            'total' => $total, 
+        ]);
     }
     public function add($id, Request $request)
     {
